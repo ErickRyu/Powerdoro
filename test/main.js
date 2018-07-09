8fee3cc5e82911f333c72c805b7abe852326e73b
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2018-07-09 21:49:25 +0900

    Rename the test case

--- test/main.js
+++ test/main.js
@@ -5,7 +5,7 @@ const getPrettyTime = require('../getPrettyTime');
 
 
 describe('getPrettyTime()', function() {
-    it('returns a formatted string', function() {
+    it('changes 1000 ms to 00:01', function() {
         assert.equal(getPrettyTime(1000), '00:01');
     })
 })
