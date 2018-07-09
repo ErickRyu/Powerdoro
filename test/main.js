5fa58bdb38f38144934c5729c5bde2a47337c556
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2018-07-09 21:50:00 +0900

    Assert that 0ms is changed to 00:00

--- test/main.js
+++ test/main.js
@@ -5,6 +5,10 @@ const getPrettyTime = require('../getPrettyTime');
 
 
 describe('getPrettyTime()', function() {
+    it('changes 0 ms to 00:00', function() {
+        assert.equal(getPrettyTime(0), '00:00');
+    })
+
     it('changes 1000 ms to 00:01', function() {
         assert.equal(getPrettyTime(1000), '00:01');
     })
