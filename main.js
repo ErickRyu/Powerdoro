51d4947049f2990fc401f8efd4cb8318dbe2c6d4
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2018-07-11 01:22:36 +0900

    Inline trayBounds

--- main.js
+++ main.js
@@ -94,10 +94,9 @@ const platforms = {
 };
 
 const getTrayWindowPosition= () => {
-    const trayBounds = tray.getBounds()
     const externalDisplay = getExternalDisplayThreashold();
 
-    return calcPosition(platforms[process.platform].calcRelativeY, trayBounds, trayWindow.getBounds().width, externalDisplay.y);
+    return calcPosition(platforms[process.platform].calcRelativeY, tray.getBounds(), trayWindow.getBounds().width, externalDisplay.y);
 }
 
 
