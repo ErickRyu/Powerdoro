cfaa4dbad8ce5442d8af575508e1117efabd942f
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2018-07-11 00:53:05 +0900

    Inline calcRelativeYOnWin32()

--- calcPosition.js
+++ calcPosition.js
@@ -3,10 +3,9 @@ module.exports = function(platform, trayBounds, windowWidth, externalDisplayY) {
     const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowWidth / 2));
     // Position window 4 pixels vertically below the tray icon
     let y = externalDisplayY;
-    const calcRelativeYOnWin32 = (trayBounds) => trayBounds.y - (3 + 120); //Todo: Extract constant and replace to trayWindow's height
     const calcRelativeY = {
         darwin: (trayBounds) => Math.round(trayBounds.y + trayBounds.height + 3),
-        win32: calcRelativeYOnWin32,
+        win32: (trayBounds) => trayBounds.y - (3 + 120), //Todo: Extract constant and replace to trayWindow's height
     }
     y += calcRelativeY[platform](trayBounds);
     return { x: x, y: y };
