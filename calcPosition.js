b644a58c94446e216b3fde14f6ff45aa564b5fc3
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2018-07-11 01:34:45 +0900

    Change the function to a arrow function

--- calcPosition.js
+++ calcPosition.js
@@ -1,6 +1,4 @@
-module.exports = function(calcRelativeY, trayBounds, windowWidth, externalDisplayY) {
-    return {
-        x: Math.round(trayBounds.x + (trayBounds.width / 2) - (windowWidth / 2)),
-        y: externalDisplayY + calcRelativeY(trayBounds),
-     };
-}
+module.exports = (calcRelativeY, trayBounds, windowWidth, externalDisplayY) => ({
+    x: Math.round(trayBounds.x + (trayBounds.width / 2) - (windowWidth / 2)),
+    y: externalDisplayY + calcRelativeY(trayBounds),
+ });
