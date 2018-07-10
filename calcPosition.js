570a31df5fc016175d2ad1a4832c762c52e6c157
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2018-07-11 01:34:09 +0900

    Inline x

--- calcPosition.js
+++ calcPosition.js
@@ -1,5 +1,6 @@
 module.exports = function(calcRelativeY, trayBounds, windowWidth, externalDisplayY) {
-    // Center window horizontally below the tray icon
-    const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowWidth / 2));
-    return { x: x, y: externalDisplayY + calcRelativeY(trayBounds) };
+    return {
+        x: Math.round(trayBounds.x + (trayBounds.width / 2) - (windowWidth / 2)),
+        y: externalDisplayY + calcRelativeY(trayBounds),
+     };
 }
