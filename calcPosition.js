25e3fa57f427f9b09e5343a55c6bb56390b6c816
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2018-07-11 00:44:04 +0900

    Use a ternary operator

--- calcPosition.js
+++ calcPosition.js
@@ -3,11 +3,8 @@ module.exports = function(platform, trayBounds, windowWidth, externalDisplayY) {
     const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowWidth / 2));
     // Position window 4 pixels vertically below the tray icon
     let y = externalDisplayY;
-    if (platform == 'darwin') {
-        y += Math.round(trayBounds.y + trayBounds.height + 3);
-    }
-    else {
-        y += trayBounds.y - (3 + 120); //Todo: Extract constant and replace to trayWindow's height
-    }
+    y += platform == 'darwin'
+        ? Math.round(trayBounds.y + trayBounds.height + 3)
+        : trayBounds.y - (3 + 120); //Todo: Extract constant and replace to trayWindow's height
     return { x: x, y: y };
 }
