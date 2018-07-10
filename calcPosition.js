cb22b48e623025b6961fc1b833ab5c98f0401a44
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2018-07-11 00:37:04 +0900

    Remove duplication

--- calcPosition.js
+++ calcPosition.js
@@ -3,12 +3,11 @@ module.exports = function(platform, trayBounds, windowWidth, externalDisplayY) {
     const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowWidth / 2));
     // Position window 4 pixels vertically below the tray icon
     let y = 0;
+    y = externalDisplayY;
     if (platform == 'darwin') {
-        y = externalDisplayY;
         y += Math.round(trayBounds.y + trayBounds.height + 3);
     }
     else {
-        y = externalDisplayY;
         y += trayBounds.y - (3 + 120); //Todo: Extract constant and replace to trayWindow's height
     }
     return { x: x, y: y };
