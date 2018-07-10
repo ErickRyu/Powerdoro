482f9a328694f9f66308bf4d21a1f7018752442f
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2018-07-11 00:49:28 +0900

    Extract calcRelativeYOnMax()

--- calcPosition.js
+++ calcPosition.js
@@ -3,8 +3,9 @@ module.exports = function(platform, trayBounds, windowWidth, externalDisplayY) {
     const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowWidth / 2));
     // Position window 4 pixels vertically below the tray icon
     let y = externalDisplayY;
+    const calcRelativeYOnMac = (trayBounds) => Math.round(trayBounds.y + trayBounds.height + 3);
     y += platform == 'darwin'
-        ? Math.round(trayBounds.y + trayBounds.height + 3)
+        ? calcRelativeYOnMac(trayBounds)
         : trayBounds.y - (3 + 120); //Todo: Extract constant and replace to trayWindow's height
     return { x: x, y: y };
 }
