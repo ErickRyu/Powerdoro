3509174b72d4d21a13d35ea0189213fd3179b29a
ErickRyu <rkggmdii@gmail.com>
2018-09-18 12:04:05 +0900

    Rename menu.html to tray-window.html

--- main.js
+++ main.js
@@ -136,7 +136,7 @@ const createTrayWindow = () => {
         'node-integration': false
     })
     
-    trayWindow.loadURL('file://' + __dirname + '/menu.html');
+    trayWindow.loadURL('file://' + __dirname + '/tray-window.html');
 
     // Hide the window when it loses focus
     trayWindow.on('blur', () => {
