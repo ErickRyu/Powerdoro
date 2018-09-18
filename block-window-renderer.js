3fdf9c458ea712654c1f7c045a36500b97b7a0f7
ErickRyu <rkggmdii@gmail.com>
2018-09-18 12:21:49 +0900

    align

--- block-window-renderer.js
+++ block-window-renderer.js
@@ -4,7 +4,12 @@ const {ipcRenderer} = require('electron')
 
 
 function sendRetrospect(event){
-    let retrospect = document.getElementById('retrospect').value;
-    ipcRenderer.send('retrospect-message', retrospect)
+  let retrospect = document.getElementById('retrospect').value;
+  ipcRenderer.send('retrospect-message', retrospect)
 }
 
+ipcRenderer.on('block-time-update', (event, arg) =>{ // Todo: Refactoring dupicated get element and consider using Jquery
+  document.getElementById('time').value = arg
+  document.getElementById('time').disabled = true
+  document.getElementById('submit_btn').disabled = true
+})
--- main.js
+++ main.js
@@ -21,50 +21,50 @@ let min
 let startedTime, stopedTime;
 
 var AutoLauncher = new AutoLaunch({
-    name: 'powerdoro',
-    path: '/Applications/powerdoro.app',
+  name: 'powerdoro',
+  path: '/Applications/powerdoro.app',
 })
 AutoLauncher.enable();
 
 
 function getExternalDisplayThreashold(){
-    var electronScreen = electron.screen
-    var displays = electronScreen.getAllDisplays()
-    var externalDisplay = null
-    for (var i in displays) {
-        if (displays[i].bounds.x != 0 || displays[i].bounds.y != 0) {
-            externalDisplay = displays[i]
-            break
-        }
+  var electronScreen = electron.screen
+  var displays = electronScreen.getAllDisplays()
+  var externalDisplay = null
+  for (var i in displays) {
+    if (displays[i].bounds.x != 0 || displays[i].bounds.y != 0) {
+      externalDisplay = displays[i]
+      break
     }
-    return externalDisplay?  {x: externalDisplay.bounds.x, y: externalDisplay.bounds.y} : {x: 0, y:0}
+  }
+  return externalDisplay?  {x: externalDisplay.bounds.x, y: externalDisplay.bounds.y} : {x: 0, y:0}
 }
 
 
 function createBlockConcentrationWindow () {
-    let displayThreashold = getExternalDisplayThreashold()
+  let displayThreashold = getExternalDisplayThreashold()
 
-    let xThreshold = displayThreashold.x
-    let yThreshold = displayThreashold.y
+  let xThreshold = displayThreashold.x
+  let yThreshold = displayThreashold.y
 
-    let setting = {
-        x: xThreshold,
-        y: yThreshold,
-        fullscreen: true,
-        frame:false,
-        alwaysOnTop: true,
-        movable: false,
-    }
-    mainWindow = new BrowserWindow(setting)
-    let mainWindowPath = path.join(__dirname, 'block-window.html')
-    mainWindow.loadFile(mainWindowPath)
+  let setting = {
+    x: xThreshold,
+    y: yThreshold,
+    fullscreen: true,
+    frame:false,
+    alwaysOnTop: true,
+    movable: false,
+  }
+  mainWindow = new BrowserWindow(setting)
+  let mainWindowPath = path.join(__dirname, 'block-window.html')
+  mainWindow.loadFile(mainWindowPath)
 
-    mainWindow.setClosable(false);
+  mainWindow.setClosable(false);
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        mainWindow = null
-    })
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
 }
 
 
@@ -77,44 +77,44 @@ function stopTimer(){
 
 
 function getMilliSecFor(min, sec){
-    let ms = ((min * 60) + sec) * ONE_MILLISEC
-    ms = Math.ceil(ms / ONE_MILLISEC) * ONE_MILLISEC; // Round up by one millisecond
-    return ms
+  let ms = ((min * 60) + sec) * ONE_MILLISEC
+  ms = Math.ceil(ms / ONE_MILLISEC) * ONE_MILLISEC; // Round up by one millisecond
+  return ms
 }
 
 
 function startTimer(min, sec){
-    startedTime = moment().format('HH:mm');
-    let ms = getMilliSecFor(min, sec)
+  startedTime = moment().format('HH:mm');
+  let ms = getMilliSecFor(min, sec)
+  updateTray(tray, trayWindow.webContents, ms);
+  intervalObj = setInterval(()=>{
+    ms -= ONE_MILLISEC
     updateTray(tray, trayWindow.webContents, ms);
-    intervalObj = setInterval(()=>{
-        ms -= ONE_MILLISEC
-        updateTray(tray, trayWindow.webContents, ms);
-        if(ms <= 0){ // Todo: Refactoring duplicated stop timer action
-          stopTimer()
-       }
+    if(ms <= 0){ // Todo: Refactoring duplicated stop timer action
+      stopTimer()
+    }
 
-    }, ONE_MILLISEC)
+  }, ONE_MILLISEC)
 }
-
 
 
 const createTray = () => {
-    let iconPath = path.join(__dirname, 'appicon.png')
-    tray = new Tray(iconPath)
-    tray.on('click', function (event) {
-        toggleWindow()
-    })
+  let iconPath = path.join(__dirname, 'appicon.png')
+  tray = new Tray(iconPath)
+  tray.on('click', function (event) {
+    toggleWindow()
+  })
 }
 
+
 const platforms = {
-    darwin: {
-        calcRelativeY: (trayBounds) => Math.round(trayBounds.y + trayBounds.height + 3),
-        hide: (app) => app.dock.hide(),
-        quit: (app) => app.quit(),
-    },
+  darwin: {
+    calcRelativeY: (trayBounds) => Math.round(trayBounds.y + trayBounds.height + 3),
+      hide: (app) => app.dock.hide(),
+      quit: (app) => app.quit(),
+  },
     win32: {
-        calcRelativeY: (trayBounds) => trayBounds.y - (3 + 120), //Todo: Extract constant and replace to trayWindow's height
+      calcRelativeY: (trayBounds) => trayBounds.y - (3 + 120), //Todo: Extract constant and replace to trayWindow's height
         hide: (app) => {},
         quit: (app) => {},
     }
@@ -123,80 +123,81 @@ const platforms = {
 
 // Creates window & specifies its values
 const createTrayWindow = () => {
-    trayWindow = new BrowserWindow({
-        width: 220,
-        height: 160,
-        show: false,
-        frame: false,
-        fullscreenable: false,
-        resizable: false,
-        transparent: true,
-        movable: false,
-        closable: false,
-        'node-integration': false
-    })
-    
-    trayWindow.loadURL('file://' + __dirname + '/tray-window.html');
+  trayWindow = new BrowserWindow({
+    width: 220,
+    height: 160,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: true,
+    movable: false,
+    closable: false,
+    'node-integration': false
+  })
 
-    // Hide the window when it loses focus
-    trayWindow.on('blur', () => {
-        if (!trayWindow.webContents.isDevToolsOpened()) {
-            trayWindow.hide()
-        }
-    })
+  trayWindow.loadURL('file://' + __dirname + '/tray-window.html');
+
+  // Hide the window when it loses focus
+  trayWindow.on('blur', () => {
+    if (!trayWindow.webContents.isDevToolsOpened()) {
+      trayWindow.hide()
+    }
+  })
 }
 
 
 const toggleWindow = () => {
-    if (trayWindow.isVisible()) {
-        trayWindow.hide()
-    } else {
-        showTrayWindow()
-    }
+  if (trayWindow.isVisible()) {
+    trayWindow.hide()
+  } else {
+    showTrayWindow()
+  }
 }
 
 
 const showTrayWindow = () => {
-    const position = calcTrayWindowXy(
-        platforms[process.platform].calcRelativeY(tray.getBounds()),
-        tray.getBounds(),
-        trayWindow.getBounds().width,
-        getExternalDisplayThreashold().y
-    );
+  const position = calcTrayWindowXy(
+    platforms[process.platform].calcRelativeY(tray.getBounds()),
+    tray.getBounds(),
+    trayWindow.getBounds().width,
+    getExternalDisplayThreashold().y
+  );
 
-    trayWindow.setPosition(position.x, position.y, false)
-    trayWindow.show()
-    trayWindow.focus()
+  trayWindow.setPosition(position.x, position.y, false)
+  trayWindow.show()
+  trayWindow.focus()
+}
+
+
+var appendRetrospect = function(retrospect) {
+  let retroDirPath = path.join(homedir + '/Desktop/retrospect/')
+  if(!fs.existsSync(retroDirPath)){
+    fs.mkdir(retroDirPath)
+  }
+  let retroPath = path.join(retroDirPath + moment().format('YYYY_MM_DD') + '.txt') //Todo: Refacor with es5 syntax
+
+  let ms = getMilliSecFor(min, 0)
+  let prettyTime = getPrettyTime(ms)
+
+  let history = `[${startedTime}-${stopedTime}] [${prettyTime}] : ${retrospect}`
+  fs.appendFile(retroPath, history + '\n', (err)=>{
+    if(err){
+      console.log(err)
+      throw err
+    }
+  })
+  mainWindow.setClosable(true)
+  mainWindow.close()
 }
 
 
 ipcMain.on('asynchronous-message', (event, arg) => {
-    min = arg
-    startTimer(arg, 0)
-    trayWindow.hide();
+  min = arg
+  startTimer(arg, 0)
+  trayWindow.hide();
 })
 
-
-var appendRetrospect = function(retrospect) {
-    let retroDirPath = path.join(homedir + '/Desktop/retrospect/')
-    if(!fs.existsSync(retroDirPath)){
-      fs.mkdir(retroDirPath)
-    }
-    let retroPath = path.join(retroDirPath + moment().format('YYYY_MM_DD') + '.txt') //Todo: Refacor with es5 syntax
-    
-    let ms = getMilliSecFor(min, 0)
-    let prettyTime = getPrettyTime(ms)
-    
-    let history = `[${startedTime}-${stopedTime}] [${prettyTime}] : ${retrospect}`
-    fs.appendFile(retroPath, history + '\n', (err)=>{
-        if(err){
-            console.log(err)
-            throw err
-        }
-    })
-    mainWindow.setClosable(true)
-    mainWindow.close()
-}
 
 ipcMain.on('retrospect-message', (event, arg) => {
   appendRetrospect(arg)
@@ -210,7 +211,7 @@ ipcMain.on('stop-message', (event, arg) => {
 
 
 ipcMain.on('exit-app', (event, arg) =>{
-      app.exit()
+  app.exit()
 })
 
 
@@ -218,23 +219,23 @@ platforms[process.platform].hide(app);
 
 
 app.on('ready', ()=>{
-    createTray()
-    createTrayWindow()
+  createTray()
+  createTrayWindow()
 })
 
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    platforms[process.platform].quit(app);
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  platforms[process.platform].quit(app);
 })
 
 
 app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createBlockConcentrationWindow()
-    }
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createBlockConcentrationWindow()
+  }
 })
